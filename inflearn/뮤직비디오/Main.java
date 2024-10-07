5ba7d6358975faf8c03600d249488c863528182e
gojsh <gojsh95@gmail.com>
2024-10-07 23:05:00 +0900

    241007

--- inflearn/뮤직비디오/Main.java
+++ inflearn/뮤직비디오/Main.java
@@ -5,12 +5,27 @@ import java.util.Scanner;
 
 class Main {
     public int count(int[] arr, int capacity) {
-
+        int cnt = 1, sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else sum += x;
+        }
+        return cnt;
     }
 
     public int solution(int n, int m, int[] arr) {
         int answer = 0;
-
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else lt = mid + 1;
+        }
         return answer;
     }
 
@@ -20,7 +35,7 @@ class Main {
         int n = kb.nextInt();
         int m = kb.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) arr[i] = kb.nextInt();
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
         System.out.println(T.solution(n, m, arr));
     }
 }
