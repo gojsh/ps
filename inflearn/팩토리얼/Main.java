d21e9a0aac210f84e60e372632296d699097239a
gojsh <gojsh95@gmail.com>
2024-10-08 11:57:18 +0900

    241008

---
+++ inflearn/팩토리얼/Main.java
@@ -0,0 +1,13 @@
+package ps.inflearn.팩토리얼;
+
+class Main {
+    public int DFS(int n) {
+        if (n == 1) return 1;
+        else return n * DFS(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        System.out.println(T.DFS(5));
+    }
+}
