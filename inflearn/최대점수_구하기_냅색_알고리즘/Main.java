b5129639d2df562b5625df0f97b79d8d3a2a5546
gojsh <gojsh95@gmail.com>
2024-10-12 02:25:35 +0900

    241012

---
+++ inflearn/최대점수_구하기_냅색_알고리즘/Main.java
@@ -0,0 +1,22 @@
+package ps.inflearn.최대점수_구하기_냅색_알고리즘;
+
+import java.util.Scanner;
+
+class Main {
+    static int[] dy;
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] dy = new int[m + 1];
+        for (int i = 0; i < n; i++) {
+            int ps = kb.nextInt();
+            int pt = kb.nextInt();
+            for (int j = m; j >= pt; j--) {
+                dy[j] = Math.max(dy[j], dy[j - pt] + ps);
+            }
+        }
+        System.out.println(dy[m]);
+    }
+}
