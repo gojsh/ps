45e69cb11dfe427c56544b65e96779dbe0482177
gojsh <gojsh95@gmail.com>
2024-10-14 23:35:26 +0900

    241014

--- programmers/외계어_사전/Solution.java
+++ programmers/외계어_사전/Solution.java
@@ -6,10 +6,10 @@ class Solution {
     public int solution(String[] spell, String[] dic) {
         int answer = 2;
         int index;
-        for (int i = 0; i < dic.length; i++) {
+        for (String s : dic) {
             index = 0;
-            for (int j = 0; j < spell.length; j++) {
-                if (dic[i].contains(spell[j])) index++;
+            for (String string : spell) {
+                if (s.contains(string)) index++;
                 if (index == spell.length) {
                     answer = 1;
                 }
