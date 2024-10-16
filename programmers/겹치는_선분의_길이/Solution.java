1d39b8b654b624d6d057ad51c940bc32d9911468
gojsh <gojsh95@gmail.com>
2024-10-16 12:27:48 +0900

    241016

--- programmers/겹치는_선분의_길이/Solution.java
+++ programmers/겹치는_선분의_길이/Solution.java
@@ -1,18 +1,36 @@
 package ps.programmers.겹치는_선분의_길이;
 
+import java.util.Scanner;
+
 class Solution {
     public int solution(int[][] lines) {
-        int[] arr = new int[200];
         int answer = 0;
+        int[] count = new int[201];
 
-        for (int i = 0; i < lines.length; i++)
-            for (int j = lines[i][0] + 100; j < lines[i][1] + 100; j++)
-                arr[j]++;
+        for (int[] i : lines) {
+            int start = i[0] + 100;
+            int end = i[1] + 100;
 
-        for (int i = 0; i < 200; i++)
-            if (arr[i] > 1)
-                answer++;
+            for (int k = start; k < end; k++) {
+                count[k] += 1;
+            }
+        }
 
+        for (int i : count) {
+            if (i > 1) answer++;
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int[][] lines = new int[3][2];
+        for (int i = 0; i < lines.length; i++) {
+            for (int j = 0; j < lines[i].length; j++) {
+                lines[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(s.solution(lines));
     }
 }
