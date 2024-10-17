063b6be399f7016d57e8924810fbadba401abc90
gojsh <gojsh95@gmail.com>
2024-10-17 15:04:44 +0900

    241017

--- programmers/로그인_성공/Solution.java
+++ programmers/로그인_성공/Solution.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "";
-        for (int i = 0; i < db.length; i++) {
-            if (id_pw[0].equals(db[i][0])) {
-                if (id_pw[1].equals(db[i][1])) answer = "login";
+        for (String[] strings : db) {
+            if (id_pw[0].equals(strings[0])) {
+                if (id_pw[1].equals(strings[1])) answer = "login";
                 else answer = "wrong pw";
             }
         }
