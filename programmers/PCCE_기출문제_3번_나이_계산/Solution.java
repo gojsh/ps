a0e3eb66df17af1c4203987d21fc5c9463a11a32
gojsh <gojsh95@gmail.com>
2024-10-20 15:38:50 +0900

    241020

---
+++ programmers/PCCE_기출문제_3번_나이_계산/Solution.java
@@ -0,0 +1,19 @@
+package ps.programmers.PCCE_기출문제_3번_나이_계산;
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        String age_type = sc.next();
+
+        int answer = 0;
+
+        if (age_type.equals("Korea")) answer = 2030 - year + 1;
+
+        else if (age_type.equals("Year")) answer = 2030 - year;
+
+        System.out.println(answer);
+    }
+}
