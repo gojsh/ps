11568736afb0e090f4705f17f7c2293c498b533b
gojsh <gojsh95@gmail.com>
2024-10-20 15:33:09 +0900

    241020

--- programmers/다음에_올_숫자/Solution.java
+++ programmers/다음에_올_숫자/Solution.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class Solution {
     public int solution(int[] common) {
-        int answer = 0;
+        int answer;
 
         if ((common[1] - common[0]) == (common[2] - common[1]))
             answer = common[common.length - 1] + (common[1] - common[0]);
