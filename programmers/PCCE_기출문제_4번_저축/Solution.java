c45426063d8bbeb4a91b7f68ef812312f89a4f07
gojsh <gojsh95@gmail.com>
2024-10-20 15:42:18 +0900

    241020

---
+++ programmers/PCCE_기출문제_4번_저축/Solution.java
@@ -0,0 +1,26 @@
+package ps.programmers.PCCE_기출문제_4번_저축;
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int before = sc.nextInt();
+        int after = sc.nextInt();
+        int money = start;
+        int month = 1;
+
+        while (money < 70) {
+            money += before;
+            month++;
+        }
+
+        while (money < 100) {
+            money += after;
+            month++;
+        }
+
+        System.out.println(month);
+    }
+}
