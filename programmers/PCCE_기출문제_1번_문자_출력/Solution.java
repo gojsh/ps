bf209e1b3524dc5a84348491a039bbbfc5fadd5d
gojsh <gojsh95@gmail.com>
2024-10-21 17:31:06 +0900

    241021

--- programmers/PCCE_기출문제_1번_문자_출력/Solution.java
+++ programmers/PCCE_기출문제_1번_문자_출력/Solution.java
@@ -2,7 +2,7 @@ package ps.programmers.PCCE_기출문제_1번_문자_출력;
 
 public class Solution {
     public static void main(String[] args) {
-        String message = "Let\'s go! ";
+        String message = "Let's go! ";
         System.out.println("3\n2\n1");
         System.out.println(message);
     }
