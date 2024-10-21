2dab5e646b0ad92b11989bc2b3d81f67d72c7a38
gojsh <gojsh95@gmail.com>
2024-10-21 15:01:27 +0900

    241021

--- programmers/PCCE_기출문제_6번_가채점/Solution.java
+++ programmers/PCCE_기출문제_6번_가채점/Solution.java
@@ -1,5 +1,7 @@
 package ps.programmers.PCCE_기출문제_6번_가채점;
 
+import java.util.Scanner;
+
 class Solution {
     public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
         int num_student = numbers.length;
@@ -13,6 +15,21 @@ class Solution {
             }
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++) numbers[i] = kb.nextInt();
+        int[] our_score = new int[n];
+        for (int i = 0; i < our_score.length; i++) our_score[i] = kb.nextInt();
+        int k = kb.nextInt();
+        int[] score_list = new int[k];
+        for (int i = 0; i < score_list.length; i++) score_list[i] = kb.nextInt();
+
+        for (String i : s.solution(numbers, our_score, score_list)) System.out.print(i);
     }
 }
 
