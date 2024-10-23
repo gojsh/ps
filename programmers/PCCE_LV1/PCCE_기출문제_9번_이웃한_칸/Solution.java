ba6aa38847ee2eabeee7e6cdcdf99c666bea0934
gojsh <gojsh95@gmail.com>
2024-10-23 16:05:42 +0900

    241023

--- programmers/PCCE_LV1/PCCE_기출문제_9번_이웃한_칸/Solution.java
+++ programmers/PCCE_LV1/PCCE_기출문제_9번_이웃한_칸/Solution.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 class Solution {
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
 
         String color = board[h][w];
 
