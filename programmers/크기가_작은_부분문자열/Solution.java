984ed2dc42df42f8de2bbced797156b4dac0363b
gojsh <gojsh95@gmail.com>
2024-10-26 15:54:06 +0900

    241026

---
+++ programmers/크기가_작은_부분문자열/Solution.java
@@ -0,0 +1,27 @@
+package ps.programmers.크기가_작은_부분문자열;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        int len = p.length();
+        Long pLong = Long.parseLong(p);
+
+        for (int i = 0; i < t.length() - len + 1; i++) {
+            String str = t.substring(i, i + len);
+            if (Long.parseLong(str) <= pLong) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        String t = kb.next();
+        String p = kb.next();
+        System.out.println(s.solution(t, p));
+    }
+}
