135a7c4990298585c67f8bf5262ed2410642f27a
gojsh <gojsh95@gmail.com>
2024-10-30 16:05:23 +0900

    241030

---
+++ programmers/최소직사각형/Solution.java
@@ -0,0 +1,32 @@
+package ps.programmers.최소직사각형;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max_w = 0;
+        int max_l = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            int w = Math.max(sizes[i][0], sizes[i][1]);
+            int l = Math.min(sizes[i][0], sizes[i][1]);
+            max_w = Math.max(max_w, w);
+            max_l = Math.max(max_l, l);
+        }
+        answer = max_w * max_l;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] sizes = new int[n][2];
+        for (int i = 0; i < sizes.length; i++) {
+            for (int j = 0; j < sizes[i].length; j++) {
+                sizes[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(s.solution(sizes));
+    }
+}
