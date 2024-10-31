25827b1a9894e3200cba0d653d041b11434438cf
gojsh <gojsh95@gmail.com>
2024-10-31 12:38:49 +0900

    241031

---
+++ programmers/문자열을_정수로_바꾸기/Solution.java
@@ -0,0 +1,18 @@
+package ps.programmers.문자열을_정수로_바꾸기;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(s.solution(str));
+    }
+}
