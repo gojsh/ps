a5e7147f9ff073ba2abccc345c7ebf1219dac258
gojsh <gojsh95@gmail.com>
2024-11-02 00:19:21 +0900

    241102

---
+++ programmers/짝수와_홀수/Solution.java
@@ -0,0 +1,19 @@
+package ps.programmers.짝수와_홀수;
+
+import java.util.Scanner;
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if (num % 2 == 0) answer = "Even";
+        else answer = "Odd";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int num = kb.nextInt();
+        System.out.println(s.solution(num));
+    }
+}
