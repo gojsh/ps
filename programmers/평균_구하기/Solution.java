5c97300aaf75894baa0c26046e5906bcb607c107
gojsh <gojsh95@gmail.com>
2024-11-03 16:36:19 +0900

    241103

--- programmers/평균_구하기/Solution.java
+++ programmers/평균_구하기/Solution.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-        for (int i = 0; i < arr.length; i++) {
-            answer += arr[i];
+        for (int i : arr) {
+            answer += i;
         }
         answer = answer / arr.length;
         return answer;
