4091cfa359eeb3f8bd279015bdcfa4a2db3543e0
gojsh <gojsh95@gmail.com>
2024-11-05 17:35:04 +0900

    241105

---
+++ programmers/하샤드_수/Solution.java
@@ -0,0 +1,25 @@
+package ps.programmers.하샤드_수;
+
+import java.util.Scanner;
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int o = x;
+        int sum = 0;
+        while (x != 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        if (o % sum == 0) return answer;
+        else answer = false;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int x = kb.nextInt();
+        s.solution(x);
+    }
+}
