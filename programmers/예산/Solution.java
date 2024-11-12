e66259670b65903f515644f46d08e50c0a78851b
gojsh <gojsh95@gmail.com>
2024-11-13 05:32:13 +0900

    241113

--- programmers/예산/Solution.java
+++ programmers/예산/Solution.java
@@ -7,9 +7,9 @@ class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
         Arrays.sort(d);
-        for(int i=0; i<d.length; i++) {
-            budget -= d[i];
-            if(budget < 0) break;
+        for (int i : d) {
+            budget -= i;
+            if (budget < 0) break;
             answer++;
         }
         return answer;
