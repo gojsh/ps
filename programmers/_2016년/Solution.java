2e70f06bae4130b61493e612b5cd7d0d07c31f12
gojsh <gojsh95@gmail.com>
2024-11-18 04:37:01 +0900

    241118

--- programmers/_2016년/Solution.java
+++ programmers/_2016년/Solution.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
+        String answer;
         String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
         int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
         int day = 0;
