f403c53ec145f70c761b8409275e821b32bbaeac
gojsh <gojsh95@gmail.com>
2024-11-19 06:53:31 +0900

    241119

---
+++ programmers/소수_찾기/Solution.java
@@ -0,0 +1,24 @@
+package ps.programmers.소수_찾기;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] ch = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            if (ch[i] == 0) {
+                answer++;
+                for (int j = i; j <= n; j = j + i) ch[j] = 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        System.out.println(s.solution(n));
+    }
+}
