a48c75a5e366f02d9adca905324fcfa0e60483ae
gojsh <gojsh95@gmail.com>
2024-11-20 04:58:17 +0900

    241120

--- programmers/옹알이_2/Solution.java
+++ programmers/옹알이_2/Solution.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] str = {"aya", "ye", "woo", "ma"};
         for (int i = 0; i < babbling.length; i++) {
-            String s = babbling[i];
-
             if (babbling[i].contains("ayaaya")
                     || babbling[i].contains("yeye")
                     || babbling[i].contains("woowoo")
@@ -20,7 +17,7 @@ class Solution {
             babbling[i] = babbling[i].replace("woo", " ");
             babbling[i] = babbling[i].replace("ma", " ");
             babbling[i] = babbling[i].replace(" ", "");
-            if(babbling[i].isEmpty()) answer++;
+            if (babbling[i].isEmpty()) answer++;
         }
         return answer;
     }
