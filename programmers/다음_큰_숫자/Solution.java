e265d909c3f69e25f18471284e0e9674b94f701b
gojsh <gojsh95@gmail.com>
2024-11-28 03:21:01 +0900

    241128

---
+++ programmers/다음_큰_숫자/Solution.java
@@ -0,0 +1,24 @@
+package ps.programmers.다음_큰_숫자;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(int n) {
+        int answer = n + 1;
+        int cnt = Integer.bitCount(n);
+        while (true) {
+            if (cnt == Integer.bitCount(answer)) {
+                break;
+            }
+            answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        System.out.println(s.solution(n));
+    }
+}
