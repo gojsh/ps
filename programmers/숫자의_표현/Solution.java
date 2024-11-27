07d7ef42bc58a7e44162da1febcedbcef1bcb1d0
gojsh <gojsh95@gmail.com>
2024-11-28 02:34:22 +0900

    241128

---
+++ programmers/숫자의_표현/Solution.java
@@ -0,0 +1,28 @@
+package ps.programmers.숫자의_표현;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                }
+                if (sum > n) break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        s.solution(n);
+    }
+}
