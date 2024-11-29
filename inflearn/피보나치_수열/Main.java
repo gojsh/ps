3d312a94ecec1a771db5ec97a2b9ded0da4b413b
gojsh <gojsh95@gmail.com>
2024-11-29 09:24:54 +0900

    241129

--- inflearn/피보나치_수열/Main.java
+++ inflearn/피보나치_수열/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class Main {
     public int[] solution(int n) {
         int[] answer = new int[n];
-        answer[0] = 1;
+        answer[0] = 0;
         answer[1] = 1;
         for (int i = 2; i < n; i++) {
             answer[i] = answer[i - 2] + answer[i - 1];
---
+++ programmers/피보나치_수/Solution.java
@@ -0,0 +1,25 @@
+package ps.programmers.피보나치_수;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+        }
+        answer = arr[n] % 1234567;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        System.out.println(s.solution(n));
+    }
+}
+
